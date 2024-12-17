9aec2b23047217d45ad9fd2d23a1eeddf3527f84
blaisebarrette <blaiseb@mac.com>
2024-12-17 12:06:44 -0500

    More cleanup

--- deploy/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-print("Content-type: text/html\n")
-print("<html>")
-print("<head><title>Test CGI</title></head>")
-print("<body>")
-print("<h1>Hello from Python!</h1>")
-print("</body>")
-print("</html>") 
